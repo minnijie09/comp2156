f127124c62c81e9907b0d6176e928cb8b5049fbd
Minji Buendia <minji.buendia@georgebrown.ca>
2025-02-09 23:19:59 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Student ID:101098057 </h1>
+      <h1>Student ID: Minji Buendia </h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developed By: Minji Buendia
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
